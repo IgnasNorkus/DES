0874a41cfce4f70aae9e3f0e136955d3a01f9873
Ignas Norkus <ignorkus@yahoo.com>
2021-03-11 17:47:26 +0200

    DES ECB Decryption

--- DES/Form1.cs
+++ DES/Form1.cs
@@ -25,7 +25,7 @@ namespace DES
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            decryptedTexttb.Text = DecryptECB(dTexttb.Text, dKeytb.Text);
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -40,20 +40,55 @@ namespace DES
 
         public string EncryptECB(string source, string key)
         {
-            TripleDESCryptoServiceProvider desCryptoProvider = new TripleDESCryptoServiceProvider();
-            MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider();
+            try
+            {
+                TripleDESCryptoServiceProvider desCryptoProvider = new TripleDESCryptoServiceProvider();
+                MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider();
 
-            byte[] byteHash;
-            byte[] byteBuff;
+                byte[] byteHash;
+                byte[] byteBuff;
 
-            byteHash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(key));
-            desCryptoProvider.Key = byteHash;
-            desCryptoProvider.Mode = CipherMode.ECB; //CBC, CFB
-            byteBuff = Encoding.UTF8.GetBytes(source);
+                byteHash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(key));
+                desCryptoProvider.Key = byteHash;
+                desCryptoProvider.Mode = CipherMode.ECB;
+                byteBuff = Encoding.UTF8.GetBytes(source);
 
-            string encoded =
-                Convert.ToBase64String(desCryptoProvider.CreateEncryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
-            return encoded;
+                string encoded =
+                    Convert.ToBase64String(desCryptoProvider.CreateEncryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
+                return encoded;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
+
+        public static string DecryptECB(string encodedText, string key)
+        {
+            try
+            {
+                TripleDESCryptoServiceProvider desCryptoProvider = new TripleDESCryptoServiceProvider();
+                MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider();
+
+                byte[] byteHash;
+                byte[] byteBuff;
+
+                byteHash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(key));
+                desCryptoProvider.Key = byteHash;
+                desCryptoProvider.Mode = CipherMode.ECB;
+                byteBuff = Convert.FromBase64String(encodedText);
+
+                string plaintext = Encoding.UTF8.GetString(desCryptoProvider.CreateDecryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
+                return plaintext;
+            }
+            catch (System.Security.Cryptography.CryptographicException)
+            {
+
+                throw;
+            }
+
+        }
+
     }
 }
